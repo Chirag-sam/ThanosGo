caf124c122db62717284648a823d071bbc310a5a
Chirag Samtani <chirag@exotel.in>
2019-04-27 15:25:26 +0530

    Add recyclerView for events

--- app/src/main/java/com/angelhack/thanosgo/fragments/EventsFragment.kt
+++ app/src/main/java/com/angelhack/thanosgo/fragments/EventsFragment.kt
@@ -6,16 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.angelhack.thanosgo.EventsAdapter
 import com.angelhack.thanosgo.R
+import com.angelhack.thanosgo.models.Event
+import kotlinx.android.synthetic.main.fragment_events.*
 
 class EventsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
+
+        val rootView = inflater.inflate(R.layout.fragment_activities, container, false)
+        val events = listOf<Event>(Event("", "Garbage", "sdsdsdsd"),
+                                   Event("", "Plant Trees", "dsdsdsdsdsd"))
+        eventsRecyclerView.adapter = EventsAdapter(listOf()) {
+
+            rootView.toast("Clicked! ${it.title}")
+
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activities, container, false)
+        return rootView
+    }
+
+    fun itemClick(event: Event) {
+
     }
 
 
---
+++ app/src/main/java/com/angelhack/thanosgo/models/Event.kt
@@ -0,0 +1,3 @@
+package com.angelhack.thanosgo.models
+
+data class Event(val imageURL: String, val title: String, val desc: String)
