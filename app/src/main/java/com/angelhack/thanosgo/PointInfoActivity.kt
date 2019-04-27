0d7e5ddc1ac78ec35ea9aea68014a345b8c8323c
Abdul Kadir <ak98.narutokun@gmail.com>
2019-04-28 03:22:25 +0530

    Add AWS storage for picture

--- app/src/main/java/com/angelhack/thanosgo/PointInfoActivity.kt
+++ app/src/main/java/com/angelhack/thanosgo/PointInfoActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_point_info2.*
 import org.jetbrains.anko.toast
@@ -14,6 +15,15 @@ import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+import com.amazonaws.mobile.client.UserStateDetails
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobile.client.Callback
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.internal.Constants
+
 
 class PointInfoActivity : AppCompatActivity() {
     val REQUEST_IMAGE_CAPTURE = 1
@@ -28,11 +38,23 @@ class PointInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_point_info2)
 
+        AWSMobileClient.getInstance().initialize(applicationContext, object : Callback<UserStateDetails> {
+            override fun onResult(userStateDetails: UserStateDetails) {
+                Log.i("Point information", "AWSMobileClient initialized. User State is " + userStateDetails.userState)
+            }
+
+            override fun onError(e: Exception) {
+                Log.e("Point information", "Initialization error.", e)
+            }
+        })
+
         clickPicBtn.setOnClickListener {
             //toast("Clicked!")
             dispatchTakePictureIntent()
         }
     }
+
+    private var imageFilePath  = ""
 
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
@@ -52,6 +74,11 @@ class PointInfoActivity : AppCompatActivity() {
                         "com.angelhack.thanosgo.fileprovider",
                         it
                     )
+
+                    imageFilePath = photoFile.absolutePath
+
+
+                    Log.d("Point Info", "${photoFile.absolutePath}")
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
                 }
@@ -77,7 +104,54 @@ class PointInfoActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
            toast("success")
-            //imageView.setImageBitmap(imageBitmap)
+            uploadWithTransferUtility()
         }
     }
+
+    fun uploadWithTransferUtility() {
+        val transferUtility = TransferUtility.builder()
+            .context(applicationContext)
+            .awsConfiguration(AWSMobileClient.getInstance().configuration)
+            .s3Client(AmazonS3Client(AWSMobileClient.getInstance()))
+            .build()
+        val uploadObserver = transferUtility.upload(
+            "public/${File(imageFilePath).name}",
+            File(imageFilePath))
+        // Attach a listener to the observer to get state update and progress notifications
+        uploadObserver.setTransferListener(object: TransferListener {
+            override fun onStateChanged(id:Int, state: TransferState) {
+                if (TransferState.COMPLETED === state)
+                {
+                    // Handle a completed upload.
+                    try {
+                        val s3AvatarURL = AWSS3Util().getS3Client(applicationContext).getResourceUrl("thanosgoapp3e716f86386f42b9952ddffbb9dfcb16-dev", File(imageFilePath).name)
+                        Log.i("Point Info", s3AvatarURL)
+                    } catch (e: Exception) {
+//                        if (avatarURL != null) {
+//                            s3AvatarURL = avatarURL!!
+//                        }
+                    }
+                }
+            }
+            override fun onProgressChanged(id:Int, bytesCurrent:Long, bytesTotal:Long) {
+                val percentDonef = (bytesCurrent.toFloat() / bytesTotal.toFloat()) * 100
+                val percentDone = percentDonef.toInt()
+                Log.d("YourActivity", ("ID:" + id + " bytesCurrent: " + bytesCurrent
+                        + " bytesTotal: " + bytesTotal + " " + percentDone + "%"))
+            }
+            override fun onError(id:Int, ex:Exception) {
+                // Handle errors
+            }
+        })
+        // If you prefer to poll for the data, instead of attaching a
+        // listener, check for the state and progress in the observer.
+        if (TransferState.COMPLETED === uploadObserver.state)
+        {
+            // Handle a completed upload.
+        }
+        Log.d("YourActivity", "Bytes Transferred: " + uploadObserver.getBytesTransferred())
+        Log.d("YourActivity", "Bytes Total: " + uploadObserver.getBytesTotal())
+    }
+
+
 }
