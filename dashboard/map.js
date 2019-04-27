eb0239a54cb7cd0cb95bc6ed51c445c939fc596a
Arjun Mahishi <arjun.mahishi@gmail.com>
2019-04-28 02:32:37 +0530

    Added click event listener on map

--- dashboard/map.js
+++ dashboard/map.js
@@ -1,15 +1,18 @@
 var map;
+
 function initMap() {
     getCurrentLocation(pos => {
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: pos.coords.latitude, lng: pos.coords.longitude},
-            zoom: 16,
+            zoom: 2,
         });
 
-        new google.maps.Marker({position: {
-            lat: pos.coords.latitude,
-            lng: pos.coords.longitude
-        }, map: map});
+        plotEvents(map, eventsList)
+
+        google.maps.event.addListener(map, 'click', function(event) {
+            console.log(event.latLng.lat())
+            console.log(event.latLng.lng())
+        });
     }) 
 }
 
@@ -18,3 +21,27 @@ const getCurrentLocation = (position) => {
         navigator.geolocation.getCurrentPosition(position);
     }
 }
+
+/*
+    event: {
+        name,
+        catagory,
+        desc,
+        location
+    }
+*/
+const plotEvents = (map, events) => events.forEach(event => plotMarker(map, event.location))
+
+const plotMarker = (map, pos) => new google.maps.Marker({position: {
+        lat: pos.lat, 
+        lng: pos.lng
+    }, 
+    map: map
+});
+
+const eventsList = [
+    {location: {lat:32.806671, lng:-86.791130}},
+    {location: {lat:42.755966, lng:-107.302490}},
+    {location: {lat:37.769337, lng:-78.169968}},
+]
+
